6d7c0a6201aec2070ec1ee364ed86baaf2d686cf
Daniel Alejandro Aguado Mendez <daalagme@gmail.com>
2025-09-09 18:33:26 -0600

    90 - mostrar datos seleccionados del subpopup, limpiar errores de validaciones

--- DjangoProject/static/js/addusuario.js
+++ DjangoProject/static/js/addusuario.js
@@ -5,11 +5,28 @@ function guardarDatos() {
 function abrirModal(nombre) {
     if (nombre == 'TipoUsuario') {
         setinner("exampleModalLabel", "Buscar o Seleccionar Tipo Usuario")
-        pintar('/usuario/listartipousuario/', "divmodalTabla", undefined,false, false,"IIDTIPOUSUARIO",false,undefined, true, "NOMBRE")
+        pintar('/usuario/listartipousuario/', "divmodalTabla", undefined, false, false, "IIDTIPOUSUARIO", false, undefined, true, "NOMBRE")
     } else {
         setinner("exampleModalLabel", "Buscar o Seleccionar Persona")
-        pintar('/persona/listarPersonaAsync/', "divmodalTabla", undefined,false, false,"IDPERSONA",false,undefined, true, "NOMBRECOMPLETO")
+        pintar('/persona/listarPersonaAsync/', "divmodalTabla", undefined, false, false, "IDPERSONA", false, undefined, true, "NOMBRECOMPLETO")
 
 
     }
+}
+
+function AsignarValores(id, valor) {
+    var titulo = getinner("exampleModalLabel")
+    document.getElementById("bntCerrarModal").click()
+
+    if (titulo.includes("Usuario")) {
+        set("txtnombretipousuario", valor)
+        set("tipousrhidden", id)
+
+    } else {
+        set("txtnombrepersona", valor)
+        set("personahidden", id)
+    }
+    alert(titulo)
+    alert(id)
+    alert(valor)
 }
--- DjangoProject/static/js/generic.js
+++ DjangoProject/static/js/generic.js
@@ -99,7 +99,7 @@ function pintar(url, idDiv = "divTabla", idtabla = "tabla",
                     }
                     if (subpopup == true) {
 
-                        contenido += `<i class="btn btn-success" onclick="AsignarValores(${objeto[propID]}, ${objeto[propDisplay]})">
+                        contenido += `<i class="btn btn-success" onclick="AsignarValores(${objeto[propID]}, '${objeto[propDisplay]}')">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                         <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
@@ -137,6 +137,10 @@ function set(id, valor) {
 
 function setinner(id, valor) {
     document.getElementById(id).innerHTML = valor
+}
+
+function getinner(id) {
+    return document.getElementById(id).innerHTML
 }
 
 function setsrc(id, valor) {
@@ -259,6 +263,12 @@ function validarObligatorios(selectorCSS, camposObviar = [], campoAcampo = false
                             diverr.innerHTML = "<span style='color:red'> Debe ingresar el " + custom_err_msg + "</span>"
                         }
                     }
+                } else {
+                     diverr = document.getElementById("div" + control.id);
+                    if (diverr) { 
+                        diverr.innerHTML=""
+                    }
+
                 }
             }
         } else {
@@ -287,6 +297,11 @@ function validarObligatorios(selectorCSS, camposObviar = [], campoAcampo = false
                         else {
                             diverr.innerHTML = "<span style='color:red'> Debe ingresar el " + custom_err_msg + "</span>"
                         }
+                    }
+                } else {
+                    diverr = document.getElementById("div" + control.id);
+                    if (diverr) {
+                        diverr.innerHTML = ""
                     }
                 }
             }
