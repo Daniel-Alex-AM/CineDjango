6e324637b1805ef637a07c3f54ad8d778f64ade2
Daniel Alejandro Aguado Mendez <daalagme@gmail.com>
2025-09-10 17:29:43 -0600

    98 - Crear procedure guardarTipoUsuario (split string, fetch while table)

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -270,9 +270,9 @@ as
 begin
 if @idpersona=''
 	insert into persona(DNI, NOMBRE, APPATERNO, APMATERNO, FECHANAC, DIRECCION, 
-						TELEFONOFIJO, TELEFONOCELULAR, IDSEXO, BHABILITADO)
+						TELEFONOFIJO, TELEFONOCELULAR, IDSEXO, BHABILITADO, BTIENEUSUARIO)
 			values(@dni, @nombre, @apaterno, @amaterno, @fechanac, @direccion, 
-					@telefonofijo, @telefonocelular, @idsexo, 1)
+					@telefonofijo, @telefonocelular, @idsexo, 1, 0)
 
 else
 	update PERSONA
@@ -489,6 +489,7 @@ begin
 	on f.IDCINE = c.IDCINE
 	inner join SALA s
 	on f.IDSALA = s.IDSALA
+	WHERE f.BHABILITADO = 1
 
 end
 
@@ -612,19 +613,19 @@ begin
 				set @idfuncionnew=@@IDENTITY
 				/*COMMIT*/
 
-				WHILE @indexcol <= @numcols
+				WHILE @indexrow <= @numrows
 					begin
-						set @indexrow = 1
-						while @indexrow <= @numrows
+						set @indexcol = 1
+						while @indexcol <= @numcols
 							begin
 								INSERT INTO BUTACA(IDFUNCION, IDBUTACA, INDICEFILA, INDICECOLUMNA, BLIBRE, BHABILITADO)
 								VALUES(@idfuncionnew, @contador, @indexrow, @indexcol, 1, 1)
 
-								set @indexrow = @indexrow+1
+								set @indexcol = @indexcol+1
 								set @contador = @contador+1
 							end
 
-						set @indexcol = @indexcol+1
+						set @indexrow = @indexrow+1
 					end
 			/*COMMIT*/
 			end
@@ -719,10 +720,308 @@ begin
 	ORDER BY INDICEFILA ASC, INDICECOLUMNA ASC
 end
 
-exec recuperarButacas 29
+exec recuperarButacas 24
 
 
 
 UPDATE BUTACA
 SET BLIBRE = 0
 WHERE INDICEFILA = 1
+
+/******************************************************************/
+UPDATE BUTACA
+SET BHABILITADO = 0
+WHERE IDFUNCION = 26
+
+create procedure deshabilitarButaca
+@idfuncion int,
+@idbutaca int
+
+as
+begin
+	UPDATE BUTACA
+	SET BHABILITADO = 0
+	WHERE IDBUTACA = @idbutaca and IDFUNCION = @idfuncion
+end
+
+
+select * from BUTACA where IDFUNCION=24
+exec deshabilitarButaca 24, 11
+
+/********************************************/
+CREATE procedure listarTipoUsuario
+as
+begin
+	select IIDTIPOUSUARIO, NOMBRE
+	from TIPOUSUARIO
+	WHERE BHABILITADO = 1
+end
+
+exec listarPersona
+
+
+-- sera transaccion porque guardara/INSERT en Usuario y modificara/UPDATE Persona
+-- TRANSACCION - o se ejecutan las 2 o no se ejecuta ninguna
+alter procedure guardarUsuario
+@idusuario int,
+@nombreusuario varchar(100),
+@pwd varchar(100),
+@idtipouser int,
+@idpersona int
+
+as
+begin
+	begin transaction
+		begin try
+			IF @idusuario = 0
+				BEGIN
+					INSERT INTO USUARIO(NOMBREUSUARIO, CONTRA, IDTIPOUSUARIO, IDPERSONA, BHABILITADO)
+					VALUES(@nombreusuario, @pwd, @idtipouser, @idpersona, 1)
+
+					UPDATE PERSONA
+					SET BTIENEUSUARIO = 1
+					WHERE IDPERSONA = @idpersona
+				END
+			ELSE
+				BEGIN
+					UPDATE USUARIO
+					SET NOMBREUSUARIO = @nombreusuario, IDTIPOUSUARIO=@idtipouser
+					WHERE IDUSUARIO = @idusuario
+				END
+
+			COMMIT TRANSACTION
+
+			SELECT 1 --RETURN 1 SI TODO OK
+
+		end try
+		
+		begin catch
+			ROLLBACK TRANSACTION
+			SELECT 0 --RETURN 0 SI FALLA
+		end catch
+
+end
+
+
+select * from USUARIO
+select * from PERSONA where IDPERSONA = 15
+select * from TIPOUSUARIO
+ALTER TABLE PERSONA
+ADD BTIENEUSUARIO INT
+
+UPDATE PERSONA
+SET BTIENEUSUARIO = 0
+
+-- IDUSUARIO, username, passwod, tipousuario, idpersona
+exec guardarUsuario 0, 'felipes0', 'password', 2, 1
+
+--------------------------------------------------------------
+CREATE PROCEDURE listarPersonaSinUser
+as
+begin
+	select IDPERSONA as IDPERSONA, DNI as DNI, NOMBRE+' '+APPATERNO+' '+APMATERNO as NOMBRECOMPLETO ,
+		convert(varchar, FECHANAC, 103) as FECHANACIMIENTO
+	from PERSONA
+	WHERE BHABILITADO=1 AND BTIENEUSUARIO=0
+end
+----------------------------------------------------------------------
+select * from BUTACA WHERE IDFUNCION=32
+
+exec listarCine
+
+exec listarFuncion
+/************************************************************/
+CREATE PROCEDURE buscarFuncionPorCine
+@idcine int
+
+as
+begin
+	SELECT f.IDFUNCION, convert(varchar, f.FECHAFUNCION) AS FECHAFUNCION, c.NOMBRE as CINE, p.TITULO AS PELICULA, s.NOMBRE AS SALA
+	FROM FUNCION f inner join PELICULA p ON f.IDPELICULA = p.IDPELICULA
+	INNER JOIN CINE c ON f.IDCINE = c.IDCINE
+	INNER JOIN SALA s ON f.IDSALA = s.IDSALA
+	WHERE f.BHABILITADO = 1 and c.IDCINE = @idcine
+end
+
+/**********************************************/
+
+create procedure listarUsuarioTipoPersona
+as
+begin
+
+	SELECT u.IDUSUARIO, u.NOMBREUSUARIO, tu.NOMBRE AS NOMBRETIPOUSUARIO, p.NOMBRE+' '+p.APPATERNO+' '+p.APMATERNO as NOMBRECOMPLETO
+	FROM USUARIO u
+	INNER JOIN TIPOUSUARIO tu on u.IDTIPOUSUARIO = tu.IIDTIPOUSUARIO
+	INNER JOIN PERSONA p ON u.IDPERSONA = p.IDPERSONA
+	WHERE u.BHABILITADO = 1
+
+end
+
+
+alter procedure recuperarUsuarioById
+@idusuario int
+as
+begin
+	SELECT u.IDUSUARIO, u.NOMBREUSUARIO, tu.NOMBRE AS NOMBRETIPOUSUARIO, p.NOMBRE+' '+p.APPATERNO+' '+p.APMATERNO as NOMBRECOMPLETO, u.IDTIPOUSUARIO AS IDTIPOUSR
+	FROM USUARIO u
+	INNER JOIN TIPOUSUARIO tu on u.IDTIPOUSUARIO = tu.IIDTIPOUSUARIO
+	INNER JOIN PERSONA p ON u.IDPERSONA = p.IDPERSONA
+	WHERE u.BHABILITADO = 1 AND u.IDUSUARIO = @idusuario
+end
+
+----------------------------------------------------------------
+SELECT * FROM TIPOUSUARIO
+SELECT * FROM PAGINA
+SELECT * FROM PAGINATIPOUSUARIO
+
+INSERT INTO PAGINA(MENSAJE, FUNCION, NOMBREAPLICACION, BHABILITADO)
+VALUES('Pais', 'listar/', 'pais', 1)
+
+INSERT INTO PAGINA(MENSAJE, FUNCION, NOMBREAPLICACION, BHABILITADO)
+VALUES('Tipo Cine', 'listar/', 'tipocine', 1)
+
+INSERT INTO PAGINA(MENSAJE, FUNCION, NOMBREAPLICACION, BHABILITADO)
+VALUES('Cines', 'listar/', 'cine', 1)
+
+create procedure listarPaginas
+as
+begin
+	SELECT IIDPAGINA, MENSAJE
+	FROM PAGINA
+	WHERE BHABILITADO = 1
+end
+
+
+create procedure listarPaginas
+as
+begin
+	SELECT IIDPAGINA, MENSAJE
+	FROM PAGINA
+	WHERE BHABILITADO = 1
+end
+
+CREATE procedure listarTipoUsuarioBD
+as
+begin
+	select IIDTIPOUSUARIO, NOMBRE,DESCRIPCION
+	from TIPOUSUARIO
+	WHERE BHABILITADO = 1
+end
+--------------------------------------------------
+----- INSERTAR EN TIPO USUARIO -------------------
+--------------------------------------------------
+SELECT * FROM TIPOUSUARIO
+SELECT * FROM PAGINATIPOUSUARIO
+
+select * from dbo.splitstring('1*2*3')
+
+ALTER FUNCTION dbo.splitstring ( @stringToSplit VARCHAR(MAX) )
+RETURNS
+ @returnList TABLE ([Name] [nvarchar] (500))
+AS
+BEGIN
+
+ DECLARE @name NVARCHAR(255)
+ DECLARE @pos INT
+
+ WHILE CHARINDEX('*', @stringToSplit) > 0
+ BEGIN
+  SELECT @pos  = CHARINDEX('*', @stringToSplit)  
+  SELECT @name = SUBSTRING(@stringToSplit, 1, @pos-1)
+
+  INSERT INTO @returnList 
+  SELECT @name
+
+  SELECT @stringToSplit = SUBSTRING(@stringToSplit, @pos+1, LEN(@stringToSplit)-@pos)
+ END
+
+ INSERT INTO @returnList
+ SELECT @stringToSplit
+
+ RETURN
+END
+
+
+
+SELECT * FROM dbo.splitstringcustom('1','*')
+
+CREATE FUNCTION dbo.splitstringcustom ( @stringToSplit VARCHAR(MAX), @delimiter nchar(1) )
+RETURNS @returnList TABLE ([Name] [nvarchar] (500))
+AS
+BEGIN
+
+ DECLARE @name NVARCHAR(255)
+ DECLARE @pos INT
+
+ WHILE CHARINDEX(@delimiter, @stringToSplit) > 0
+ BEGIN
+  SELECT @pos  = CHARINDEX(@delimiter, @stringToSplit)  
+  SELECT @name = SUBSTRING(@stringToSplit, 1, @pos-1)
+
+  INSERT INTO @returnList 
+  SELECT @name
+
+  SELECT @stringToSplit = SUBSTRING(@stringToSplit, @pos+1, LEN(@stringToSplit)-@pos)
+ END
+
+ INSERT INTO @returnList
+ SELECT @stringToSplit
+
+ RETURN
+END
+
+---------------------------------------------------------------------------------
+alter procedure guardarTipoUsuario
+@idtipousr int,
+@nombretipousr varchar(100),
+@descripcion varchar(100),
+@opciones varchar(100)
+
+as
+begin
+	declare @idgeneradonewtipousr int
+	declare @idpagina int
+
+	begin transaction
+		begin try
+			if @idtipousr =0
+			BEGIN
+				insert into TIPOUSUARIO(NOMBRE, DESCRIPCION, BHABILITADO)
+				VALUES(@nombretipousr, @descripcion,1)
+				set @idgeneradonewtipousr=@@IDENTITY --Es el ID que generó la operación anterior
+
+				declare Tabla cursor for select * from splitstringcustom(@opciones,'*')
+				OPEN Tabla
+				-- RECORRER REGISTRO POR REGISTRO (en buble)
+				FETCH NEXT FROM Tabla into @idpagina
+				WHILE @@FETCH_STATUS=0
+					BEGIN
+						INSERT INTO PAGINATIPOUSUARIO(IIDTIPOUSUARIO, IIDPAGINA, BHABILITADO)
+						VALUES(@idgeneradonewtipousr, @idpagina, 1)
+
+						FETCH NEXT FROM Tabla into @idpagina
+					END
+			END
+
+			ELSE
+			BEGIN
+				UPDATE TIPOUSUARIO
+				SET NOMBRE=@nombretipousr, DESCRIPCION=@descripcion
+				WHERE IIDTIPOUSUARIO = @idtipousr
+			END
+
+
+			COMMIT TRANSACTION
+			SELECT 1
+		end try
+
+		begin catch
+			select 0
+		end catch
+
+end
+
+
+EXEC guardarTipoUsuario 0, 'Jefe', 'El Jefe', '1*2*3'
+EXEC guardarTipoUsuario 4, 'ELJefe', 'El Jefe Jefe', '1*2*3'
+select * from tipousuario
